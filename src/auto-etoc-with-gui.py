c248ca0a7387c694bf48ae100e4e77c1550709c3
benbutterworth <code@benbutterworth.org>
2025-05-15 21:44:54 +0100

    refactor - port euto-etoc changes to auto-etoc-with-gui

--- src/auto-etoc-with-gui.py
+++ src/auto-etoc-with-gui.py
@@ -3,21 +3,44 @@ from bs4 import BeautifulSoup
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.scrolled import ScrolledText
+import re
 
-def check_url(url):
-    # add error check that URL is springerlink and an articlehere
-    # if url doesn't have https://link.springer.com/article then throw error
+def check_url(url, target="article"):
+    # Define the appropriate url format for each type of site to scrape
+    if target == "article":
+        regex = r"link\.springer\.com\/article\/10\.1007"
+    elif target == "issue":
+        regex = r"link\.springer.com\/journal\/125\/volumes-and-issues"
+    else:
+        raise TypeError("Invalid target - please specify what the source should be")
+    # Check that url matches these formats
+    if not re.search(regex, url):
+        raise ValueError("URL does not point to valid source of etoc information.")
     pass
 
 def clean_author_text(author):
+    trailing_chars = "0123456789,&" + " "
     if "\xa0" in author:
-        return(author.split("\xa0")[0].strip("0123456789, "))
+        return(author.split("\xa0")[0].strip(trailing_chars))
     else:
-        return(author.strip("0123456789, "))
+        return(author.strip(trailing_chars))
+
+def get_author_line(authors):
+    clean_names = [clean_author_text(author.text) for author in authors]
+    # Capture up fronts or other non-article exceptions
+    if len(clean_names) == 0: 
+        return ""
+    elif len(clean_names) == 1:
+        return clean_names[0]
+    # Capture "on behalf of..." or "for..." exceptions
+    elif re.search(r"(on\s)|(for\s)", clean_names[-1]):
+        return ", ".join(clean_names[:-2]) + f" & {clean_names[-2]} {clean_names[-1]}"
+    # Connect to last author with and (standard response)
+    else:
+        return ", ".join(clean_names[:-1]) + f" & {clean_names[-1]}"
 
 # Scrape HTML webpage from springerlink here
 def get_website_soup(url, give_main=True):
-    check_url(url)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     if give_main:
@@ -28,35 +51,28 @@ def get_website_soup(url, give_main=True):
   
 # Extract article information from HTML
 def extract_article_info(soup):
-    data = {}
-    # Find the title
-    data["title"] = soup.find_all("h1", class_="c-article-title")[0].text
-    # Find article type and open access
-    article_info = soup.find_all("li", class_="c-article-identifiers__item")
-    data["type"] = article_info[0].text
-    data["open-access"] = article_info[1].text == '\nOpen access\n'
-    # Cycle through all authors
+    # Find article metadata and list of authors
+    article_title = soup.find_all("h1", class_="c-article-title")[0].text
+    article_info = soup.find_all("li", class_="c-article-identifiers__item") 
     authors = soup.find_all("li", class_="c-article-author-list__item")
-    authors_names = [clean_author_text(author.text) for author in authors]
-
-    # need to do some extra checking here for last item
-    # Note if "on behalf of" or other exceptions
-    # add "&" symbol between last two
-    data["authors"] = ", ".join(authors_names)
-    data["link"] = ""
+    # Create dictionary containing data
+    data = {
+        "title": article_title,
+        "type": article_info[0].text,
+        "open-access": article_info[1].text == '\nOpen access\n',
+        "authors": get_author_line(authors),
+        "link": "" 
+    }
     return data
 
 
 def get_etoc_entry(article_info): #variant of print_etoc_entry
-    entry = "\n"
-    if article_info["link"]:
-        entry += article_info["link"] + "\n"
-    entry += article_info["title"] + "\n"
-    entry += article_info["authors"] + "\n"
-    if article_info["type"]!="Article":
-        entry += "(" + article_info["type"] + ")\n"
-    if article_info["open-access"]:
-        entry += "(Open Access)\n"
+    article_type = f"\n({article_info["type"]})" if article_info["type"]!="Article" else ""
+    entry = f"""
+{article_info["link"]}
+{article_info["title"]}
+{article_info["authors"]}{article_type}
+{"(Open Access)" if article_info["open-access"] else ""}"""
     return entry
 
 def get_article_links_from_journal_issue(soup):
@@ -67,10 +83,12 @@ def get_article_links_from_journal_issue(soup):
     return links
 
 def generate_etoc(journal_issue_url):
+    check_url(journal_issue_url, target="issue")
     soup = get_website_soup(journal_issue_url)
     links = get_article_links_from_journal_issue(soup)
     etoc = ""
     for url in links:
+        check_url(url)
         soup = get_website_soup(url)
         article_info = extract_article_info(soup)
         article_info["link"] = url

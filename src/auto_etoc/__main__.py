80e30055ce19d49d1697850a020adb48fc82ba9c
benbutterworth <code@benbutterworth.org>
2025-10-16 15:59:34 +0100

    fix!: made cli buildable with pip
    added cli entrypoint. alert hatch i'm using `src` paradigm.
    real talk it was trial and error figuring this one out, which is
    irritating because it's a lot simpler than i suspected. such is life.

--- src/auto_etoc/__main__.py
+++ src/auto_etoc/__main__.py
@@ -1,1 +1,7 @@
-print("hello world") # this goes when you run through `python src/auto-etoc`
+from sys import stdout
+from . import cli
+
+def CLI():
+    if not stdout.isatty():
+        stdout.reconfigure(encoding="utf-16") # to catch weird letters
+    cli.app()
